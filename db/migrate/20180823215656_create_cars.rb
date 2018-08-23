c4d0861ec6982bb0451703c0335840cad6b5eef7
Daniel Carcich <danielreid@gmail.com>
2018-08-23 18:50:57 -0400

    remove extra field

--- db/migrate/20180823215656_create_cars.rb
+++ db/migrate/20180823215656_create_cars.rb
@@ -3,7 +3,6 @@ class CreateCars < ActiveRecord::Migration[5.2]
     create_table :cars do |t|
       t.string :make
       t.string :mode
-      t.string :string
       t.integer :year
 
       t.timestamps
